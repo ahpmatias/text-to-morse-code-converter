8c30ff3ef12f1eddcafcee975eec498b7c08f1f4
ahpmatias <63597072+ahpmatias@users.noreply.github.com>
2024-10-29 10:53:53 +0100

    Finish project. Implement input and conversion features.

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 from morse_dicts import text_to_morse_dict
 
-# TODO implement input function
-# TODO implement conversion function
+# implement input feature
+text = str(input('Which text do you want to convert to morse code? ').lower())
+
+# implement conversion feature
+for letter in text:
+    print(text_to_morse_dict[letter])
+
+#Test - Result for "Hello World" is:
+# h : o o o o
+# e : o
+# l : o - o o
+# l : o - o o
+# o : - - -
+
+# w : o - -
+# o : - - -
+# r : o - o
+# l : o - o o
+# d : - o o
